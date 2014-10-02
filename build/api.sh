d0bfd35407a498258547e0989326a53c067831df
Far Tseng <farrrr@gmail.com>
2014-10-02 23:40:07 +0800

    make it executable & change path

--- build/api.sh
+++ build/api.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd /home/forge/laravel.com/build/sami
+cd /home/Sites/laravel.tw/build/sami
 
-rm -rf /home/forge/laravel.com/build/sami/build
-rm -rf /home/forge/laravel.com/build/sami/cache
+rm -rf /home/Sites/laravel.tw/build/sami/build
+rm -rf /home/Sites/laravel.tw/build/sami/cache
 
 # Run API Docs
-git clone https://github.com/laravel/framework.git /home/forge/laravel.com/build/sami/laravel
+git clone https://github.com/laravel/framework.git /home/Sites/laravel.tw/build/sami/laravel
 
-php /home/forge/laravel.com/vendor/bin/sami.php update /home/forge/laravel.com/build/sami/sami.php
+php /home/Sites/laravel.tw/vendor/bin/sami.php update /home/Sites/laravel.tw/build/sami/sami.php
 
-cp -r /home/forge/laravel.com/build/sami/build/* /home/forge/laravel.com/public/api
+cp -r /home/Sites/laravel.tw/build/sami/build/* /home/Sites/laravel.tw/public/api
 
-rm -rf /home/forge/laravel.com/build/sami/build
-rm -rf /home/forge/laravel.com/build/sami/cache
+rm -rf /home/Sites/laravel.tw/build/sami/build
+rm -rf /home/Sites/laravel.tw/build/sami/cache
 
 # Cleanup
-rm -rf /home/forge/laravel.com/build/sami/laravel
+rm -rf /home/Sites/laravel.tw/build/sami/laravel
